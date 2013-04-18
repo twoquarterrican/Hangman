481d97739cfecd4615a99c6a0cb4f43f3ed67ca4
Justin D. Thomas <justmas@gmail.com>
2013-04-18 16:29:20 -0400

    dual input complete!
    added key listener and mouse listener and synchronization object so main
    thread can wait on this object and event threads can notify this object.

--- src/hangmanGame/Game.java
+++ src/hangmanGame/Game.java
@@ -3,6 +3,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.PrintStream;
 
 import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
@@ -21,6 +22,8 @@ public class Game extends ConsoleProgram {
 	private int numGuesses;
 	private String guessWord;
 	private HangmanCanvas canvas;
+	private boolean waitingForGuess = false;
+	private Object syncObject = new Object();
 	
 	public static void main(String[] args) {
 		new Game().start(args);
@@ -33,19 +36,10 @@ public class Game extends ConsoleProgram {
 		//Cannot do this in the run() method
 		canvas = new HangmanCanvas();
 		add(canvas);
-//		canvas.addMouseListener(new LetterClickListener());
-//		addKeyListener(new LetterTypeListener());
+		canvas.addMouseListener(new LetterClickListener());
+		canvas.addKeyListener(new LetterTypeListener());
 	}
-
-	public void run() {
-		while (true) {
-			play();
-			if(!PlayAgain()) break;
-		}
-		println("goodbye");
-		exit();
-	}
-
+	
 	private boolean PlayAgain() {
 		while(true) {
 			String again = readLine("Play Again? (Y/N) :");
@@ -62,6 +56,30 @@ public class Game extends ConsoleProgram {
 		}
 	}
 
+	private String getRandomWord() {
+		//get a random generator which will choose a random word below
+		RandomGenerator rgen = RandomGenerator.getInstance();
+		//valid wordNum 's are 0, 1, ..., (number of words in lexicon minus one)
+		int maxIndex = lexicon.getWordCount()-1;
+		int randomIndex = rgen.nextInt(0, maxIndex);
+		String word = lexicon.getWord(randomIndex);
+		guessWord = "";
+		for(int i = 0; i < word.length(); i++){
+			guessWord += "-";
+		}
+		return word;
+	}
+	
+	public void run() {
+		while (true) {
+			play();
+			if(!PlayAgain()) break;
+			getConsole().clear();
+		}
+		println("goodbye");
+		exit();
+	}
+
 	private void play() {
 		//getRandomWord also sets guessWord = "-----", with the appropriate number of -'s
 		secretWord = getRandomWord();
@@ -69,44 +87,51 @@ public class Game extends ConsoleProgram {
 		println("Welcome to Hangman!");
 		//static field in HangmanPic class gives total number of parts that can be drawn, make this the total number of guesses
 		numGuesses = MAX_GUESSES;
-		takeGuesses();
-		showResult();
+		//isGameOver returns true and prints loss or win message if game is over, otherwise returns false and prints nothing
+		while(!isGameOver()) {
+			requestGuess();
+		}
+	}
+
+	private void requestGuess() {
+		println("The word now looks like this: " + guessWord);
+		println("You have " + numGuesses + " guesses left.");
+		print("Choose a letter: ");
+		//event loops do nothing unless this flag is set to true
+		waitingForGuess = true;
+		canvas.requestFocusInWindow();
+		synchronized (syncObject) {
+			try {
+				syncObject.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				System.out.println("caught interrupted exception");
+			}
+		}
 	}
 	
-	private void takeGuesses() {
-		//game loop
-		while(true) {
-			println("The word now looks like this: " + guessWord);
-			println("You have " + numGuesses + " guesses left.");
-			//make sure we get just one character which has not been guessed before
-			char guess = getAValidGuess();
-			//evaluate..() updates either numGuesses or guessWord
-			evaluateValidGuess(guess);
-			//out of guesses or word guessed completely? guessWord starts off as "-----"
-			if (numGuesses == 0 || guessWord.indexOf('-') == -1) break;
-		}
-
-	}
-
-	private void showResult() {
-		if (numGuesses == 0) {
-			println("You lose.");
-			canvas.displayWord(guessWord);
-			println("The word was: " + secretWord);
-			println("You lose.");
-		} else {
-			println("You guessed the word: " + secretWord);
-			println("You win.");
-			canvas.playWinAnimation();
+	private void guessReceived(char guess) {
+		if (waitingForGuess) {
+			guess = Character.toUpperCase(guess);
+			println(guess);	
+			if ('A' <= guess && guess <= 'Z' && isNewGuess(guess)) {
+				waitingForGuess = false;
+				evaluateValidGuess(guess);
+				synchronized (syncObject) {
+					syncObject.notify();
+				}
+			} else {
+				println("You already guessed " + guess);
+				print("Choose a letter: ");
+			}
 		}
 	}
 
 	boolean isNewGuess(char guess) {
 		//did the player already guess this character?
+		//use Letter object to record this
 		Letter letter = canvas.getLetters().getLetter(guess);
 		if (letter.hasBeenGuessed()) {
-			//a repeat guess
-			println("You already guessed " + guess);
 			return false;
 		} 
 		//a new guess
@@ -115,14 +140,11 @@ public class Game extends ConsoleProgram {
 	}
 
 	private void evaluateValidGuess(char guess) {
-
-		//this method assumes guess un-guessed letter, so this method
-		//only needs to decide if numGuesses is decremented or if guessWord is updated
+		//this method assumes guess un-guessed letter, so this method only needs to decide if numGuesses is decremented or if guessWord is updated
 		
 		//by default chGuess not found in secretWord
 		boolean found = false;
-		//the index variable iterates over all positions of chGuess in secretWord.  Initialize to -1, which
-		//is the value of secretWord.indexOf(chGuess) if chGuess is not found
+		//the index variable iterates over all positions of chGuess in secretWord.  Initialize to -1, which is the value of secretWord.indexOf(chGuess) if chGuess is not found
 		int index = -1;
 		//sentinel for while loop is chGuess no longer found in substring of secretWord ranging from index+1 to the end
 		while (true) {
@@ -146,93 +168,88 @@ public class Game extends ConsoleProgram {
 		}
 	}
 	
-	//returns a guess guaranteed to be in the alphabet and not already guessed
-	private char getAValidGuess() {
-		while(true) {
-			String line;
-			line = readLine("Choose a letter: ");
-			if (line.equals("")) continue;
-			char guess = line.toUpperCase().charAt(0);
-			if ('A' <= guess && guess <= 'Z' && isNewGuess(guess)) return guess;
+	private boolean isGameOver() {
+		//out of guesses or word guessed completely? guessWord starts off as "-----"
+		if (numGuesses == 0) {
+			showLossMessage();
+			return true;
+		} else if (guessWord.indexOf('-') == -1) {
+			showWinMessage();
+			return true;
+		} else {
+			return false;
 		}
 	}
 
-	private String getRandomWord() {
-		//get a random generator which will choose a random word below
-		RandomGenerator rgen = RandomGenerator.getInstance();
-		//valid wordNum 's are 0, 1, ..., (number of words in lexicon minus one)
-		int maxIndex = lexicon.getWordCount()-1;
-		int randomIndex = rgen.nextInt(0, maxIndex);
-		String word = lexicon.getWord(randomIndex);
-		guessWord = "";
-		for(int i = 0; i < word.length(); i++){
-			guessWord += "-";
-		}
-		return word;
-	}
-	public void mousePressed(MouseEvent e) {
-		
-		System.out.println("unclassed mouse event fired");
-		String message = "mouse event at (" + e.getX() + "," + e.getY() + ")\n";
-		message += "  Letter clicked was ";
-		Letter letter = canvas.getLetters().getLetterAt(e.getX(),e.getY());
-		message += (letter == null ? "null" : letter.getCh());
-		System.out.println(message);
+	private void showLossMessage() {
+		println("You lose.");
+		canvas.displayWord(guessWord);
+		println("The word was: " + secretWord);
+		println("You lose.");
 	}
 	
-//	private class LetterClickListener implements MouseListener {
-//
-//		@Override
-//		public void mouseClicked(MouseEvent arg0) {
-//			// TODO Auto-generated method stub
-//
-//		}
-//
-//		@Override
-//		public void mouseEntered(MouseEvent e) {
-//			// TODO Auto-generated method stub
-//			
-//		}
-//
-//		@Override
-//		public void mouseExited(MouseEvent e) {
-//			// TODO Auto-generated method stub
-//			
-//		}
-//
-//		@Override
-//		public void mousePressed(MouseEvent e) {
-//			System.out.println("mouse pressed");
-//			Game.this.getWriter().write("mouse pressed");
-//			
-//		}
-//
-//		@Override
-//		public void mouseReleased(MouseEvent e) {
-//			// ignore
-//			
-//		}
-//
-//	}
-//	
-//	private class LetterTypeListener implements KeyListener {
-//		public void keyPressed(KeyEvent e) {
-//			String message = "key event:\n";
-//			message += "  Key pressed was " + e.getKeyChar();
-//			System.out.println(message);
-//		}
-//
-//		@Override
-//		public void keyReleased(KeyEvent arg0) {
-//			// ignore
-//
-//		}
-//
-//		@Override
-//		public void keyTyped(KeyEvent arg0) {
-//			// ignore
-//			System.out.println("stuff");
+	private void showWinMessage() {
+		println("You guessed the word: " + secretWord);
+		println("You win.");
+		canvas.playWinAnimation();
+	}
+	
+	private class LetterClickListener implements MouseListener {
+		
+		@Override
+		public void mousePressed(MouseEvent e) {
+			Letter letter = canvas.getLetters().getLetterAt(e.getX(),e.getY());
+			if (letter != null) guessReceived(letter.getCh());
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// ignore
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// ignore
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// ignore
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// ignore
+		}
+
+	}
+	
+	private class LetterTypeListener implements KeyListener {
+		public void keyPressed(KeyEvent e) {
+			guessReceived(e.getKeyChar());
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			// ignore
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// ignore
+		}
+	}
+	
+	//returns a guess guaranteed to be in the alphabet and not already guessed
+//	private char getAValidGuess() {
+//		while(true) {
+//			String line;
+//			line = readLine("Choose a letter: ");
+//			if (line.equals("")) continue;
+//			char guess = line.toUpperCase().charAt(0);
+//			if ('A' <= guess && guess <= 'Z' && isNewGuess(guess)) return guess;
 //		}
 //	}
 	
 }
+
